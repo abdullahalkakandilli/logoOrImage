c920236bd35b12b3507b4eafbc731257ee9bb081
Abdullah Alka Kandilli <alkakandilli@gmail.com>
2023-02-21 13:48:48 +0100

    button option

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import requests
 from transformers import AutoProcessor, CLIPModel
 import streamlit as st
 import pandas as pd
-import os
 from functionforDownloadButtons import download_button
-
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 def _max_width_():
     max_width_str = f"max-width: 1800px;"
@@ -44,24 +40,7 @@ with c2:
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         uploaded_file.seek(0)
-        for index, row in df.iterrows():
 
-            url = row['Logo']
-            image = Image.open(requests.get(url, stream=True).raw)
-
-            inputs = processor(
-
-                text=["logo", "not logo"], images=image, return_tensors="pt", padding=True
-
-            )
-
-            outputs = model(**inputs)
-
-            logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
-
-            probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
-            if (probs[0][1] > 0.40):
-                df.at[index, 'Logo'] = 'not Logo'
 
         file_container = st.expander("Check your uploaded .csv")
         file_container.write(df)
@@ -73,27 +52,85 @@ with c2:
                 👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
                 """
         )
-        os._exit(0)
+
         st.stop()
-c3, c4 = st.columns([1, 6])
 
-with c4:
+def get_values():
 
-    @st.experimental_memo
-    def convert_df(df):
-        return df.to_csv().encode("utf-8")
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    for index, row in df.iterrows():
+
+        url = row['Logo']
+        image = Image.open(requests.get(url, stream=True).raw)
+
+        inputs = processor(
+
+            text=["logo", "not logo"], images=image, return_tensors="pt", padding=True
+
+        )
+
+        outputs = model(**inputs)
+
+        logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+
+        probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
+        if (probs[0][1] > 0.40):
+            df.at[index, 'Logo'] = 'not Logo'
 
 
-    csv = convert_df(df)
 
-    st.caption("")
 
-    st.download_button(
-        label="Download results",
-        data=csv,
-        file_name="classification_results.csv",
-        mime="text/csv",
+if st.button('Get Results'):
+    result = get_values()
+
+
+from st_aggrid import GridUpdateMode, DataReturnMode, GridOptionsBuilder, AgGrid
+
+gb = GridOptionsBuilder.from_dataframe(df)
+# enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
+gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
+gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
+gridOptions = gb.build()
+
+st.success(
+    f"""
+        💡 Tip! Hold the shift key when selecting rows to select multiple rows at once!
+        """
+)
+
+response = AgGrid(
+    df,
+    gridOptions=gridOptions,
+    enable_enterprise_modules=True,
+    update_mode=GridUpdateMode.MODEL_CHANGED,
+    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+    fit_columns_on_grid_load=False,
+)
+
+df = pd.DataFrame(response["selected_rows"])
+
+st.subheader("Filtered data will appear below 👇 ")
+st.text("")
+
+st.table(df)
+
+st.text("")
+
+c29, c30, c31 = st.columns([1, 1, 2])
+
+with c29:
+
+    CSVButton = download_button(
+        df,
+        "File.csv",
+        "Download to CSV",
     )
 
-
-
+with c30:
+    CSVButton = download_button(
+        df,
+        "File.csv",
+        "Download to TXT",
+    )
